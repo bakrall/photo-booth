8a82fdd682a4ffc36b516724982e73d9f62bba76
Barbara Stadnik <barbara1stadnik@gmail.com>
2020-03-30 18:43:58 +0200

    create a link to download photo

--- public/photo-booth.js
+++ public/photo-booth.js
@@ -8,7 +8,7 @@ function getVideo() {
 	navigator.mediaDevices.getUserMedia({ video: true, audio: false }) //it returns a promise
 	.then(localMediaStream => {
 		video.srcObject = localMediaStream;
-		video.play();
+		video.play(); //once the video starts playing, it emits an event 'canplay'
 	})
 	.catch(err => {
 		console.error('you should allow camera', err);
@@ -26,4 +26,16 @@ function paintToCanvas() {
 	}, 16);
 }
 
+function takePhoto() {
+	//take the data out of canvas
+	const data = canvas.toDataURL('image/jpeg');
+	const link = document.createElement('a');
+	link.href = data;
+	link.setAttribute('download', 'image title);
+	link.textContent = 'Download Image';
+	strip.insertBefore(link, strip.firstChild);
+}
+
 getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
