9ab3e6032bdc86dc7e2794abf959c7cac0929399
Barbara Stadnik <barbara1stadnik@gmail.com>
2020-03-30 18:46:39 +0200

    correction of lack of closiong bracket

--- public/photo-booth.js
+++ public/photo-booth.js
@@ -31,7 +31,7 @@ function takePhoto() {
 	const data = canvas.toDataURL('image/jpeg');
 	const link = document.createElement('a');
 	link.href = data;
-	link.setAttribute('download', 'image title);
+	link.setAttribute('download', 'image title');
 	link.textContent = 'Download Image';
 	strip.insertBefore(link, strip.firstChild);
 }
