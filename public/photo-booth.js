849b953f21f0037cfe4808ea2ed531c7bfa8918f
Barbara Stadnik <barbara1stadnik@gmail.com>
2020-03-30 15:52:49 +0200

    paint video to canvas

--- public/photo-booth.js
+++ public/photo-booth.js
@@ -8,7 +8,22 @@ function getVideo() {
 	navigator.mediaDevices.getUserMedia({ video: true, audio: false }) //it returns a promise
 	.then(localMediaStream => {
 		video.srcObject = localMediaStream;
-	});
+		video.play();
+	})
+	.catch(err => {
+		console.error('you should allow camera', err);
+	})
+}
+
+function paintToCanvas() {
+	const width = video.videoWidth;
+	const height = video.videoHeight;
+	canvas.width = width;
+	canvas.height = height;
+
+	return setInterval(() => {
+		ctx.drawImage(video, 0, 0, width, height);
+	}, 16);
 }
 
 getVideo();
